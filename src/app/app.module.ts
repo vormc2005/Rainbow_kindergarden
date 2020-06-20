fffb557814104334d05159d9a5f024cb1cfb950d
vormc2005 <voronov.dmitry@gmail.com>
2020-06-20 18:14:57 -0400

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EducationComponent } from './education/education.component';
+import { BenefitsComponent } from './benefits/benefits.component';
+import { GamesComponent } from './games/games.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EducationComponent,
+    BenefitsComponent,
+    GamesComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule
