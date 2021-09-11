26073f5086c2e8771a7a25797c1038aa3fc70ade
vormc2005 <voronov.dmitry@gmail.com>
2021-09-11 14:11:24 -0400

    commented gallery page

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -3,13 +3,13 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router'
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { GalleryComponent } from './gallery/gallery.component';
+// import { GalleryComponent } from './gallery/gallery.component';
 
 const appRoutes: Routes =[
     {path: '', component:AboutComponent   },
     {path: 'about', component:AboutComponent    },
-    {path: 'contact', component:ContactComponent },
-    {path: 'gallery', component:GalleryComponent }
+    {path: 'contact', component:ContactComponent }
+    // {path: 'gallery', component:GalleryComponent }
 
     
 
